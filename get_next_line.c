3095bd627f90813743df17720cc2a33f54b2c179
Fabio_Araujow <araujobimmer@gmail.com>
2026-05-27 11:04:40 +0100

    under construction

--- get_next_line.c
+++ get_next_line.c
@@ -1,2 +1,8 @@
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static char	*storage_box;
+	char		*buffer;
+	size_t		b_read;
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,6 +5,8 @@ size_t	ft_strlen(char *s)
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	while (s[len])
 		len++;
 	return (len);
@@ -13,9 +15,10 @@ size_t	ft_strlen(char *s)
 char	*ft_strchr(char *s, int c)
 {
 	char	to_find;
-	char	*p;
 
 	to_find = (char)c;
+	if (!s)
+		return (NULL);
 	if (to_find == '\0')
 	{
 		while (*s)
@@ -34,7 +37,7 @@ char	*ft_strchr(char *s, int c)
 static char	*ft_strjoin_ncase(char *s)
 {
 	size_t	i;
-	char 	*buffer;
+	char	*buffer;
 
 	i = 0;
 	buffer = malloc((ft_strlen(s) + 1) * sizeof(char));
@@ -46,7 +49,7 @@ static char	*ft_strjoin_ncase(char *s)
 	return (buffer);
 }
 
-char *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*buffer;
 	size_t	i;
@@ -66,8 +69,25 @@ char *ft_strjoin(char *s1, char *s2)
 		return (NULL);
 	while (*s1)
 		buffer[i++] = *s1++;
-	while(*s2)
+	while (*s2)
 		buffer[i++] = *s2++;
 	buffer[i] = '\0';
 	return (buffer);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*sbstr;
+	size_t	i;
+
+	i = 0;
+	if (!s || (start > ft_strlen(s)))
+		return (NULL);
+	sbstr = malloc((len + 1) * sizeof(char));
+	if (!sbstr)
+		return (NULL);
+	while (i < len && s[start])
+		sbstr[i++] = s[start++];
+	sbstr[i] = '\0';
+	return (sbstr);
 }
