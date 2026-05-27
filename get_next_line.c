c358c6d3ad69a3f66d461fdddbd59feddebafebc
Fabio_Araujow <araujobimmer@gmail.com>
2026-05-27 13:10:09 +0100

    developing main function

--- get_next_line.c
+++ get_next_line.c
@@ -2,7 +2,29 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*storage_box;
-	char		*buffer;
-	size_t		b_read;
+	static char		*storage_box;
+	char			*temp;
+	char			*buffer;
+	int				b_read;
+
+	b_read = 1;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while ((b_read > 0) && (ft_strchr(storage_box, '\n') == NULL))
+	{
+			buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+			if (!buffer)
+				return (NULL);
+			b_read = read(fd, buffer, BUFFER_SIZE);
+			if (b_read == -1)
+			{
+				free(buffer);
+				return (NULL);
+			}
+			buffer[b_read] = '\0';
+			temp = storage_box;
+			storage_box = ft_strjoin(storage_box, buffer);
+			free(buffer);
+			free(temp);
+	}
 }
