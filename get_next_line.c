2d20ac0fba2d17df8117e1cc1db505e3989b7cb9
Fabio_Araujow <araujobimmer@gmail.com>
2026-05-29 11:31:59 +0100

    updating

--- get_next_line.c
+++ get_next_line.c
@@ -1,30 +1,39 @@
 #include "get_next_line.h"
 
+char	*ft_search_newliwe(int fd, char **storage_box)
+{
+	char	*buffer;
+	char	*temp;
+	int		bytes_read;
+
+	bytes_read = 1;
+	while ((bytes_read > 0) && (ft_strchr(*storage_box, '\n') == NULL))
+	{
+		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!buffer)
+			return (NULL);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		temp = *storage_box;
+		*storage_box = ft_strjoin(*storage_box, buffer);
+		free(temp);
+		free(buffer);
+	}
+	return (*storage_box);
+}
+
 char	*get_next_line(int fd)
 {
-	static char		*storage_box;
-	char			*temp;
-	char			*buffer;
-	int				b_read;
+	static char	*storage_box;
 
-	b_read = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while ((b_read > 0) && (ft_strchr(storage_box, '\n') == NULL))
-	{
-			buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-			if (!buffer)
-				return (NULL);
-			b_read = read(fd, buffer, BUFFER_SIZE);
-			if (b_read == -1)
-			{
-				free(buffer);
-				return (NULL);
-			}
-			buffer[b_read] = '\0';
-			temp = storage_box;
-			storage_box = ft_strjoin(storage_box, buffer);
-			free(buffer);
-			free(temp);
-	}
+	ft_search_new_line(fd, &storage_box);
+	if (!storage_box)
+		return (NULL);
 }
